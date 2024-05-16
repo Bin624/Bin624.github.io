ddeccb0d0012ad41c7d9dd76edb09b99c73368e2
adgxy <kekou002612@163.com>
2024-05-16 21:15:39 +0800

    Site updated: 2024-05-16 21:15:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,56 @@
-var posts=["2024/05/16/Hexo搭建进阶：Vercel部署、主题安装、基础用法/","2024/04/25/it-tools工具箱开发人员在线工具集合/","2024/04/13/lskypro/","2024/04/16/wordpress文章自动采集插件autopost-pro/","2024/04/23/wordpress添加观影和书单插件-wp-douban/","2024/04/14/安卓ssh软件-管理员之手/","2024/04/14/安卓本地部署alist-软件版/","2024/05/09/彩虹工具箱/","2024/04/22/服务器文件备份阿里云盘/","2024/04/13/每日60s早报-读懂世界/","2024/04/13/海底捞小积木/","2024/04/14/邮箱留言反馈源码/","2024/04/13/🐸牛蛙锅☺/"];function toRandomPost(){
+var posts=["2024/04/13/lskypro/","2024/05/16/Hexo搭建进阶：Vercel部署、主题安装、基础用法/","2024/04/25/it-tools工具箱开发人员在线工具集合/","2024/04/16/wordpress文章自动采集插件autopost-pro/","2024/04/23/wordpress添加观影和书单插件-wp-douban/","2024/04/14/安卓ssh软件-管理员之手/","2024/05/09/彩虹工具箱/","2024/04/14/安卓本地部署alist-软件版/","2024/04/22/服务器文件备份阿里云盘/","2024/04/13/每日60s早报-读懂世界/","2024/04/13/海底捞小积木/","2024/04/14/邮箱留言反馈源码/","2024/04/13/🐸牛蛙锅☺/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
